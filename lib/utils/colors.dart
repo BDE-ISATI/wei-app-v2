98466ab82979c1ad79e7bd6dbde92432a36a463a
Feldrise <admin@feldrise.com>
2021-08-24 23:06:47 +0200

    ✨ Add possibility to convert strings to color

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -70,3 +70,17 @@ bool colorIsDark(Color color) {
 
   return true;
 }
+
+extension ColorExtension on String {
+  Color toColor() {
+    var hexColor = replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF$hexColor";
+    }
+    if (hexColor.length == 8) {
+      return Color(int.parse("0x$hexColor"));
+    }
+
+    return const Color(0xfff70c35);
+  }
+}
