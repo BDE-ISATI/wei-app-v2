abc060ea7b3b9535389374a1b24d306bbbc9a61d
Feldrise <admin@feldrise.com>
2021-08-28 02:20:21 +0200

    🚀 Ready to deploy on Android

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,3 +1,3 @@
-const kApiBaseUrl = "https://192.168.1.14:45455/v1";
+// const kApiBaseUrl = "https://192.168.1.14:45455/v1";
 // const kApiBaseUrl = "https://isatiintegration-api.conveyor.cloud/v1";
-// const kApiBaseUrl = "https://api.integration.isati.org/v1";
+const kApiBaseUrl = "https://api.integration.isati.org/v1";
